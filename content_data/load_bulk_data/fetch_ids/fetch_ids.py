22ca03c0bc9003b3fe668ffed47922e75ffdb58d
kartikrast <kartik.keyrast@gmail.com>
2025-09-03 20:29:13 +0530

    Fixed movie_ids module to fetch both tv and movie ids using the same module

---
+++ content_data/load_bulk_data/fetch_ids/fetch_ids.py
@@ -0,0 +1,135 @@
+"""
+This file contains the functionality to fetch ids of movies or tv_shows from TMDB.
+
+It contains global constants:
+    - `CONFIG`: Configuration object to access BigQuery and TMDB settings.
+"""
+
+# external imports
+import requests
+from dotenv import load_dotenv
+from typing import List, Dict, Any
+
+# local imports
+from ...base_log import Logger
+from ..config.config import Config
+load_dotenv()
+
+CONFIG = Config()
+logger = Logger('fetch_ids').get_logger()
+
+class FetchIDs:
+    """
+    This class contains methods to fetch movie and tv_show ids from TMDB.
+    It uses the Config() module to access configuration and endpoint settings for TMDB.
+    It sets configurations as follows:
+
+        - `self.url`: TMDB API endpoint for fetching ids.
+        - `self.headers`: TMDB API headers.
+        - `self.default_params`: TMDB API default parameters.
+        - `self.page`: Page number for fetching ids.
+        - `self.start_date`: Start date for fetching ids.
+        - `self.end_date`: End date for fetching ids.
+        - `self.type`: This will tell the program which ids to fetch (movies or tv_shows). Defaults to `movies`.
+        - `self._set_params()`: Sets the parameters for fetching ids.
+        - `self.total_page_params`: Parameters for fetching total pages.
+        - `self.dynamic_params`: Parameters for fetching ids dynamically.
+    
+    #### Notes:
+
+        - The class relies on a Config helper (`config.Config`) to supply TMDB endpoint details, keeping secrets out of the source code.
+    
+    #### Example Usage:
+
+        >>> raw_movies = FetchIDs(page=1, start_date="2020-01-01", end_date="2020-12-31", type=movies)
+        >>> total_pages = raw_movies.get_total_pages()
+        >>> movies = raw_movies.fetch_movies()
+    """
+    def __init__(self, page: int = None, start_date: str = None, end_date: str = None, type:str = "movies"):
+        self.type = type
+        self.url, self.headers, self.default_params = CONFIG.get_tmdb_config(endpoint="discover", type=self.type)
+        self.page = page
+        self.start_date = start_date
+        self.end_date = end_date
+        self._set_params()
+    
+    def _set_params(self):
+        """
+        Set the parameters for fetching movies.
+
+        Args:
+            page (int): Page number for fetching movies.
+            start_date (str): Start date for fetching movies.
+            end_date (str): End date for fetching movies.
+        """
+        if self.type == "movies":
+            self.total_page_params = {
+                "page":1,
+                "primary_release_date.gte": self.start_date,
+                "primary_release_date.lte": self.end_date
+            }
+
+            self.dynamic_params = {
+                "page": self.page,
+                "primary_release_date.gte": self.start_date,
+                "primary_release_date.lte": self.end_date
+            }
+        elif self.type == "tv_shows":
+            self.total_page_params = {
+                "page":1,
+                "first_air_date.gte": self.start_date,
+                "first_air_date.lte": self.end_date
+            }
+
+            self.dynamic_params = {
+                "page": self.page,
+                "first_air_date.gte": self.start_date,
+                "first_air_date.lte": self.end_date
+            }
+    
+    def get_total_pages(self) -> int:
+        """
+        Get the total number of pages for the given date range.
+        
+        Returns:
+            int: Total number of pages.
+        
+        Examples:
+
+            >>> movie_ids = FetchIDs(page=1, start_date="2020-01-01", end_date="2020-12-31")
+            >>> total_pages = movie_ids.get_total_pages()
+            >>> tv_show_ids = FetchIDS(page=1, start_date="2020-01-01", end_date="2020-12-31", type="tv_shows")
+            >>> total_pages = tv_show_ids.get_total_pages()
+        """
+        url = self.url
+        params = CONFIG.set_tmdb_params(params=self.default_params, **self.total_page_params)
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            return response.json().get('total_pages', 1)
+        else:
+            logger.error(f"Failed to fetch total pages: {response.status_code} - {response.text}")
+            raise Exception(f"Failed to fetch total pages: {response.status_code} - {response.text}")
+    
+    def fetch_ids(self) -> List[Dict[str, Any]]:
+        """
+        Fetch ids from TMDB.
+        
+        Returns:
+
+            List[Dict[str, Any]]: List of ids having metadata.
+        
+        Examples:
+        
+            >>> movie_ids = FetchIDs(page=1, start_date="2020-01-01", end_date="2020-12-31")
+            >>> ids = movie_ids.fetch_ids()
+            >>> tv_show_ids = FetchIDS(page=1, start_date="2020-01-01", end_date="2020-12-31", type="tv_shows")
+            >>> ids = tv_show_ids.fetch_ids()
+        """
+        url = self.url
+        params = CONFIG.set_tmdb_params(params=self.default_params, **self.dynamic_params)
+        response = requests.get(url, headers=self.headers, params=params)
+        if response.status_code == 200:
+            return response.json().get('results', [])
+        else:
+            logger.error(f"Failed to fetch data: {response.status_code} - {response.text}")
+            raise Exception(f"Failed to fetch data: {response.status_code} - {response.text}")
--- content_data/load_bulk_data/fetch_ids/run_fetch_ids.py
+++ content_data/load_bulk_data/fetch_ids/run_fetch_ids.py
@@ -11,13 +11,14 @@ from typing import List, Tuple
 from concurrent.futures import ThreadPoolExecutor
 
 # local imports
-from .movie_ids import MovieIDs
+from .fetch_ids import FetchIDs
 from ...base_log import Logger 
 
-logger = Logger('run_movie_ids').get_logger()
+logger = Logger('run_fetch_ids').get_logger()
 
-class RunMovieIDs:
-    def __init__(self, year:int) -> None:
+class RunFetchIDs:
+    def __init__(self, year:int, type:str = "movies") -> None:
+        self.type = type
         self.year = year
         self.date_ranges = self._get_date_ranges()
     
@@ -42,12 +43,12 @@ class RunMovieIDs:
 
     def fetch_yearly_data(self) -> List:
         """
-        Fetch movies data for multiple date ranges.
+        Fetch ids for multiple date ranges.
         """
-        all_movies = []
+        all_ids = []
         for start_date, end_date in self.date_ranges:
-            logger.info(f"Fetching movies from {start_date} to {end_date}")
-            obj = MovieIDs(start_date=start_date, end_date=end_date)
+            logger.info(f"Fetching ids from {start_date} to {end_date}")
+            obj = FetchIDs(start_date=start_date, end_date=end_date, type=self.type)
 
             # Step-1: Fetch total pages
             total_pages = obj.get_total_pages()
@@ -57,27 +58,27 @@ class RunMovieIDs:
                 continue
             logger.info(f"Total pages to fetch: {total_pages}")
             
-            # Step-2: Fetch movies data
+            # Step-2: Fetch ids
             with ThreadPoolExecutor(max_workers=10) as executor:
-                futures = [executor.submit(MovieIDs(page=page, start_date=start_date, end_date=end_date).fetch_movies) for page in range(1, total_pages + 1)]
+                futures = [executor.submit(FetchIDs(page=page, start_date=start_date, end_date=end_date, type=self.type).fetch_ids) for page in range(1, total_pages + 1)]
                 count = 0
                 for future in futures:
                     try:
-                        movies = future.result()
-                        all_movies.extend(movies)
+                        ids = future.result()
+                        all_ids.extend(ids)
                         time.sleep(0.5)
                         count += 1  # Sleep to avoid hitting API rate limits
-                        logger.info(f"Fetched {len(movies)} movies on page {count}")
+                        logger.info(f"Fetched {len(ids)} ids on page {count}")
                     except Exception as e:
                         count += 1
-                        logger.error(f"Error on page {count} fetching movies: {e}")
+                        logger.error(f"Error on page {count} fetching ids: {e}")
             
-        if all_movies:
-            movie_ids_list = []
-            for movie in all_movies:
-                id = movie.get("id")
-                movie_ids_list.append(id)
-        return movie_ids_list
+        if all_ids:
+            ids_list = []
+            for data in all_ids:
+                id = data.get("id")
+                ids_list.append(id)
+        return ids_list
 
 
 
@@ -85,5 +86,5 @@ class RunMovieIDs:
 if __name__ == "__main__":
     year = 2024
     """Step-1: Fetch movie_ids for the year 2024"""
-    obj = RunMovieIDs(year=year)
-    movie_ids = obj.fetch_yearly_data()
+    obj = RunFetchIDs(year=year, type="tv_shows")
+    ids = obj.fetch_yearly_data()
